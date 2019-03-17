e0e7a220ea89c8aab4999f1b9f75b4e6ce487b06
dchou1618 <43254117+dchou1618@users.noreply.github.com>
2019-03-17 11:53:49 -0400

    Update linkedlst.py

--- Algorithms/datastructs/linkedlst.py
+++ Algorithms/datastructs/linkedlst.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import math, time, random, sys
+import random
 
 # Implementations of Linked Lists (collection using linked nodes)
 # DLL Disadvantages: Requires extra space for previous reference
@@ -12,7 +12,7 @@ class Node(object):
         return str(self.val)
 
 # Lists are useful in that they can assemble multiple objects into one
-# entity (object) # collection
+# entity (object) - collection
 def printLst(node):
     while node != None:
         print(node,end=" ")
@@ -45,10 +45,12 @@ class SinglyLinkedList(object):
     def __init__(self):
         self.head = None
         self.length = 0
+    # prints reversed linked list by reversing head
     def printReversed(self):
         print("[",end=" ")
         if self.head != None:
             self.head.printReversed()
+    # adds element to front of linked list - new node references head
     def addFirstElem(self,value):
         node = Node(value)
         self.head = node
@@ -58,24 +60,32 @@ class SinglyLinkedList(object):
 class DoublyLinkedList(object):
     def __init__(self):
         self.head = None
+    # newNode references head of linked list and vice versa
+    # - new head becomes the newNode
     def push(self, val):
         newNode = DoubleNode(val)
         newNode.reference = self.head
         if self.head != None:
             self.head.prevReference = newNode
         self.head = newNode
+    # if the prevNode not within list, nothing executed
     def insertAfter(self,prevNode, newVal):
         if prevNode == None:
             return
         newNode = DoubleNode()
+        # fits newNode references to be between prevNode & node 
+        # after that 
         newNode.nextReference = prevNode.nextReference
         prevNode.nextReference = newNode
         newNode.prevReference = prevNode
         if newNode.nextReference != None:
             newNode.nextReference.prevReference = newNode
+    # adds newNode to end of linked list
     def append(self, newVal):
         newNode = Node(newVal)
         newNode.nextReference = None
+        # if there head is None, newNode has None reference & newNode becomes
+        # the list's head
         if self.head == None:
             newNode.prevReference = None
             self.head = newNode
@@ -86,13 +96,15 @@ class DoublyLinkedList(object):
         last.nextReference = newNode
         newNode.prevReference = last
         return
-
+    # prints out list of nodes
     def printList(self, node):
+        # prints out list through nextReference links between el'ts
         while node.val != None:
             print("%d"%node.val,end=" ")
             last = node
             node = node.nextReference
         print()
+        # linked by prior references, we print out linked list backwards
         while last != None:
             try:
                 print("%d" %(last.val),end=" ")
@@ -100,22 +112,25 @@ class DoublyLinkedList(object):
                 pass
             last = last.prevReference
 
-class CircularNode(Node):
-    pass
-
 class CircularLinkedList(object):
     def __init__(self):
         self.head = None
+    # circular node appended to beginning of linked list
     def add(self, val):
         addedNode = CircularNode(val)
         temp = addedNode.reference = self.head
+        # temporary var temp referenced until reaching head
+        # - head references added node. 
         if self.head != None:
             while(temp.reference != self.head):
                 temp = temp.reference
             temp.reference = addedNode
         else:
+            # addedNode reference itself if no head
             addedNode.reference = addedNode
         self.head = addedNode
+    # prints the value of node of "temp" until we reach back to 
+    # head of list, then terminate printLst
     def printLst(self):
         temp = self.head
         if self.head != None:
