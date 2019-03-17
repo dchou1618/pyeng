80adbcf50fdd9ccc4642c08a11ee506a42bc1a9d
dchou1618 <43254117+dchou1618@users.noreply.github.com>
2019-03-17 12:37:01 -0400

    Update tweetscraper.py

--- Algorithms/sentiment/rospy/nlpWorkspace/src/nlp/tweetscraper.py
+++ Algorithms/sentiment/rospy/nlpWorkspace/src/nlp/tweetscraper.py
@@ -3,12 +3,15 @@
 import sys,twitterscraper
 from twitterscraper import query_tweets
 def scrapeTweets(key=sys.argv[1]):
+    # query with limit of 20 
     tweetsLst = query_tweets(key,20)
     file = open("output.txt","w")
     for tweet in query_tweets(key,20):
-        #file.write(tweet.text.encode("utf-8"))
+        # message encoded in utf-8 to handle other languages
         message = tweet.text.encode("utf-8")
+        # parse message into phrases in list
         phrases = message.split()
+        # basic removal of non-clean phrases
         for phrase in range(len(phrases)-1,-1,-1):
             if "http" in phrases[phrase]:
                 phrases.pop(phrase)
