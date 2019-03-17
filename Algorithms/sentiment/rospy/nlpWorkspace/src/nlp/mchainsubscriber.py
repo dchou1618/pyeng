788723f9bbbb46474abe3e90a35ed0358fd709fe
dchou1618 <43254117+dchou1618@users.noreply.github.com>
2019-03-17 12:39:41 -0400

    Update mchainsubscriber.py

--- Algorithms/sentiment/rospy/nlpWorkspace/src/nlp/mchainsubscriber.py
+++ Algorithms/sentiment/rospy/nlpWorkspace/src/nlp/mchainsubscriber.py
@@ -2,10 +2,10 @@
 # mchainsubscriber.py
 import rospy
 from std_msgs.msg import *
-
+# displays subscriber data to console as "Comment: data.data" where data.data is comment 
 def callback(data):
     print("Comment:",data.data)
-    
+# subscribes to topic of "markovTalker"
 def markovSubscriber():
     rospy.init_node("markovSubscriber",anonymous=True)
     rospy.Subscriber("markovTalker",String,callback)
