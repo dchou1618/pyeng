4935d9d6494677caa1e0c8fceb4aac834eee31a4
dchou1618 <43254117+dchou1618@users.noreply.github.com>
2019-03-17 12:14:17 -0400

    Update dijkstra.py

--- Algorithms/dijkstra/dijkstra.py
+++ Algorithms/dijkstra/dijkstra.py
@@ -11,6 +11,7 @@ class Edge(object):
         self.destinationNode = destinationNode
         self.cost = cost
 
+    # method to create itself (Edge)
     def create(self):
         return Edge(self.sourceNode,self.destinationNode,self.cost)
 
@@ -20,8 +21,9 @@ class Network(object):
         # confirming if all edges are in valid form
         try:
             self.edges = [edge.create() for edge in edges]
-        except:
+        except Exception as e:
             raise ValueError
+    # generates vertices of network - creates self.vertices
     @property
     def vertices(self):
         verts = set()
@@ -29,6 +31,7 @@ class Network(object):
             verts.add(edge.sourceNode)
             verts.add(edge.destinationNode)
         return verts
+    # nearest neighbors of each node
     @property
     def allClosestNodes(self):
         closestNodes = dict()
@@ -38,13 +41,14 @@ class Network(object):
             otherEnd = (edge.destinationNode, edge.cost)
             closestNodes[edge.sourceNode].add(otherEnd)
         return closestNodes
-
+    # finding pair connections in network
     def getNodePairs(n1, n2, both_ends=True):
         if both_ends:
             node_pairs = [[n1, n2], [n2, n1]]
         else:
             node_pairs = [[n1, n2]]
         return node_pairs
+    # initializes distances to be infinite & previous vertices - None
     def distsVerts(self):
         distances = dict()
         for vertex in self.vertices:
@@ -53,6 +57,7 @@ class Network(object):
         for vertex in self.vertices:
             priorVertices[vertex] = None
         return distances,priorVertices
+    # current vertex should be node closest to the visited nodes
     def getCurrVertex(self,distances,visitNodes):
         minDist = float("inf"); currVertex = list(visitNodes)[0]
         for vertex in visitNodes:
@@ -60,11 +65,14 @@ class Network(object):
                 minDist = distances[vertex]
                 currVertex = vertex
         return currVertex
+    # implements dijkstra's, checking validity 
     def dijkstra(self, origin, destination):
         assert(origin in self.vertices),"Source Node Not A Vertex"
         distances, priorVertices = self.distsVerts()
         distances[origin] = 0
         visitNodes = self.vertices.copy()
+        # while not all nodes have been visited, take closest nodes to visited
+        # nodes & find cheapest path from currVertex
         while len(visitNodes)>0:
             currVertex = self.getCurrVertex(distances,visitNodes)
             if distances[currVertex] == defaultDist:
@@ -75,7 +83,7 @@ class Network(object):
                     distances[closeNode] = otherPath
                     priorVertices[closeNode] = currVertex
             visitNodes.remove(currVertex)
-
+        # construct path from prior vertices
         path, currVertex = [], destination
         while priorVertices[currVertex] != None:
             path = [currVertex] + path
