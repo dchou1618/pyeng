c073538cff264f34fa346702cf0a3a1620b7a04c
dchou1618 <43254117+dchou1618@users.noreply.github.com>
2019-03-17 12:34:12 -0400

    Update sentiment.py

--- Algorithms/sentiment/pynlp/newsSentiment/sentiment.py
+++ Algorithms/sentiment/pynlp/newsSentiment/sentiment.py
@@ -1,5 +1,4 @@
-from wordcloud import WordCloud, STOPWORDS # because wordcloud is already a module, Python makes it
-# very easy to create a wordcloud
+from wordcloud import WordCloud, STOPWORDS 
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
@@ -13,21 +12,18 @@ import matplotlib.pyplot as plt
 ##############################################################################################################
 def wordcloud_draw(text, color = 'darkBlue'):
     word = " ".join([word for word in text.split()])
+    # basic construction of wordcloud if word is clean text
     if (not word.startswith("http")):
         wordcloud = WordCloud(stopwords=STOPWORDS,background_color=color,width=2500,height=2000).generate(word)
     plt.imshow(wordcloud, cmap=plt.cm.gray, interpolation='bilinear')
     plt.axis('off')
     plt.title("Most Common Words in Text")
     plt.show()
-
 ##############################################################################################################
-# (2) Basic Positivity Analysis - ML
-##############################################################################################################
-
-##############################################################################################################
-# (3) Nuanced Vader Analysis
+# (2) Nuanced Vader Analysis
 ##############################################################################################################
 nltk.download('vader_lexicon')
+# prints polarity scores using nl toolkit vader_lexicon
 def sentimentAnaly(text):
     sentiment_analy = SentimentIntensityAnalyzer()
     for row in range(len(text)):
