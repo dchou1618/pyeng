9b9f128fa6c68b2bc319efb6529552548466d6c3
dchou1618 <43254117+dchou1618@users.noreply.github.com>
2019-03-17 12:38:31 -0400

    Update mchainpublisher.py

--- Algorithms/sentiment/rospy/nlpWorkspace/src/nlp/mchainpublisher.py
+++ Algorithms/sentiment/rospy/nlpWorkspace/src/nlp/mchainpublisher.py
@@ -5,25 +5,23 @@ from std_msgs.msg import String
 import markovify
 from markovify import *
 textFile = sys.argv[1]
-# using markovify 
+# using markovify to create markov chain from text
 def generateMarkovifyChain(text):
     text = open(text).read()
     markovModel = markovify.Text(text,
                                  retain_original = False)
     return markovModel.make_sentence()
-
-# using 
-def automateMarkovChain():
-    pass
-
+# publishing randomly generated sentences from topic "markovTalker" at 
+# frequency of 10 hz
 def publishMarkovChain():
     pmarkov = rospy.Publisher("markovTalker",String,queue_size=10)
     rospy.init_node("markovPublish",anonymous=True)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         # rospy.loginfo(chain) publishes to the console
-        sentence = generateMarkovifyChain(textFile)
-        pmarkov.publish(sentence)
+        sentence = generateMarkovifyChain(textFile) 
+        pmarkov.publish(sentence) # publishes each sentence from textfile in
+        # first system argument
         rate.sleep()
 
 if __name__ == "__main__":
