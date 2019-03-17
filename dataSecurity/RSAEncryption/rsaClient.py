54aec627d637415e5b6b3c7e713ac965fda950db
dchou1618 <43254117+dchou1618@users.noreply.github.com>
2019-03-17 13:07:58 -0400

    Update rsaClient.py

--- dataSecurity/RSAEncryption/rsaClient.py
+++ dataSecurity/RSAEncryption/rsaClient.py
@@ -7,7 +7,7 @@ from rsaKey import *
 
 def mainClient(key):
     HOST,PORT = "127.0.0.1",65432 # localhost, port to listen on
-
+    # connects to initialized host, port and sends the key value, printing to the console the received data
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST,PORT))
         s.sendall(str.encode(key))
